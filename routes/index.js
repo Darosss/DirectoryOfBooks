1d524ef5d9c675304c571fee0cf0ac92b3031d15
Darosss <Daroing12@gmail.com>
2022-09-24 16:29:01 +0200

    Done games routes and styles

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const Book = require("../models/book");
+const Game = require("../models/game");
 
 router.get("/", async (req, res) => {
   let books;
+  let games;
   try {
-    books = await Book.find({}).sort({ createdAt: "desc" }).limit(10).exec();
+    books = await Book.find({}).sort({ createdAt: "desc" }).limit(7).exec();
+    games = await Game.find({}).sort({ createdAt: "desc" }).limit(7).exec();
   } catch {
     books = [];
+    games = [];
   }
-  res.render("index", { books: books });
+  res.render("index", { books: books, games: games });
 });
 
 module.exports = router;
